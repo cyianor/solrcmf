edff980d510e7a29f8e3614ce84208d6aabf7b11
Felix Held <felix.held@gmail.com>
2026-04-04 21:58:15 +0200

    correct type in singledispatch

--- src/solrcmf/base.py
+++ src/solrcmf/base.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import Field, dataclass, field
 from functools import singledispatch
-from typing import Protocol
+from typing import Any, Protocol
 
 from numpy import float64
 from numpy.typing import NDArray
@@ -40,7 +40,6 @@ class Constraint[IdxT](Block[IdxT]):
     """Base class for (multi-)affine constraints."""
 
 
-
 @dataclass
 class Context[
     BT: DataclassInstance,
@@ -75,14 +74,12 @@ class Context[
         )
 
 
-
 @singledispatch
 def update[
     BT: DataclassInstance,
     CT: DataclassInstance,
     PT: DataclassInstance,
-    IdxT,
-](block: Block[IdxT], ctx: Context[BT, CT, PT]):
+](block: Block[Any], ctx: Context[BT, CT, PT]):
     """Update the block variables."""
     raise NotImplementedError(f"update() not implemented for {type(block)}.")
 
@@ -92,8 +89,7 @@ def constraint[
     BT: DataclassInstance,
     CT: DataclassInstance,
     PT: DataclassInstance,
-    IdxT,
-](block: Constraint[IdxT], ctx: Context[BT, CT, PT]) -> NDArray[float64]:
+](block: Constraint[Any], ctx: Context[BT, CT, PT]) -> NDArray[float64]:
     """Return the lhs of a constraint f(x) = 0."""
     raise NotImplementedError(
         f"constraint() not implemented for {type(block)}."
@@ -105,7 +101,7 @@ def _[
     BT: DataclassInstance,
     CT: DataclassInstance,
     PT: DataclassInstance,
-](block: Constraint, ctx: Context[BT, CT, PT]):
+](block: Constraint[Any], ctx: Context[BT, CT, PT]):
     """Update the multipliers."""
     block.value += constraint(block, ctx)
 
@@ -125,7 +121,7 @@ def _[
     BT: DataclassInstance,
     CT: DataclassInstance,
     PT: ConstraintParams,
-](block: Constraint, ctx: Context[BT, CT, PT]) -> float:
+](block: Constraint[Any], ctx: Context[BT, CT, PT]) -> float:
     """Compute the contribution to the objective."""
     return (
         0.5
